5b0c06fb92f4fdb3faa68970a0770445fb330613
Andre <andre@andre-Aspire-A315-56>
2024-06-10 17:34:05 -0300

    Inverse Fourier

--- main.py
+++ main.py
@@ -9,6 +9,20 @@ from skimage.exposure import equalize_hist
 PATH_FOLHA = "images/folhas.jpg"
 PATH_FOLHA_RETICULADA = "images/folhas_Reticulada.jpg"
 
+"""
+fig, ax = plt.subplots(1,3,figsize=(15,15))
+
+    ax[0].imshow(np.log(abs(folha_image_fourier)), cmap='gray')
+    ax[0].set_title('Masked Fourier', fontsize = f_size)
+
+    ax[1].imshow(folha_image, cmap = 'gray')
+    ax[1].set_title('Greyscale Image', fontsize = f_size)
+
+    ax[2].imshow(abs(np.fft.ifft2(folha_image_fourier)), cmap='gray')
+    ax[2].set_title('Transformed Greyscale Image', fontsize = f_size)
+
+"""
+
 if __name__ == "__main__":
     # Abra a imagem
     folha_image = imread(PATH_FOLHA_RETICULADA, as_gray=True)
@@ -28,22 +42,19 @@ if __name__ == "__main__":
     # Aplicando mascara
     f_size = 15
 
-    folha_image_fourier[:580, 959:961] = 1
-    folha_image_fourier[-580:,959:961] = 1 
+    folha_image_fourier[:575, 959:961] = 1
+    folha_image_fourier[-575:,959:961] = 1 
 
-    fig, ax = plt.subplots(1,3,figsize=(15,15))
-
-    ax[0].imshow(np.log(abs(folha_image_fourier)), cmap='gray')
-    ax[0].set_title('Masked Fourier', fontsize = f_size)
-
-    ax[1].imshow(folha_image, cmap = 'gray')
-    ax[1].set_title('Greyscale Image', fontsize = f_size)
-
-    ax[2].imshow(abs(np.fft.ifft2(folha_image)), cmap='gray')
-    ax[2].set_title('Transformed Greyscale Image', fontsize = f_size)
+    plt.imshow(np.log(abs(folha_image_fourier)), cmap='gray')
+    plt.savefig("results/folha_mask_fourier.png")
 
     plt.show()
 
+    folha_image_inverse_fourier = abs(np.fft.ifft2(folha_image_fourier))
+
+    plt.imshow(folha_image_inverse_fourier, cmap='gray')
+    plt.savefig("results/folha_inverse_fourier.png")
+    
 
 
 
